7d95bee5963ae8b71cc24f6e20f7122cee26b37b
ronnel <jayjayjomoc@gmail.com>
2026-09-14 16:31:22 +0800

    Remove Crossmeet gallery labels

--- src/App.jsx
+++ src/App.jsx
@@ -509,7 +509,7 @@ function App() {
                       onClick={() => setLightboxIndex(index)}
                     >
                       <img src={item.image} alt={item.title} loading="lazy" />
-                      {activeGalleryCategory !== 'tambike' && <span>{item.title}</span>}
+                      {activeGalleryCategory === 'units' && <span>{item.title}</span>}
                     </motion.button>
                   ))}
                 </motion.div>
